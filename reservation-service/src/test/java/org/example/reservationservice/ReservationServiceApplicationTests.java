3e9b042f450877536ae4ce6c463efd4d30afa509
julita.zamroczynska <julita.zamroczynska@icloud.com>
2025-05-11 12:56:22 +0200

    new services

---
+++ reservation-service/src/test/java/org/example/reservationservice/ReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.reservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
