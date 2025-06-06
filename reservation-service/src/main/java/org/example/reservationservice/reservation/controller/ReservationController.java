b7481d820b9264c33415d311db1d46d313783feb
weronika <weronikakoterba31@wp.pl>
2025-06-07 01:12:08 +0200

    working kubernetess

--- reservation-service/src/main/java/org/example/reservationservice/reservation/controller/ReservationController.java
+++ reservation-service/src/main/java/org/example/reservationservice/reservation/controller/ReservationController.java
@@ -4,8 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.example.reservationservice.reservation.dto.ReservationDTO;
 import org.example.reservationservice.reservation.model.Reservation;
 import org.example.reservationservice.reservation.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.example.reservationservice.reservation.security.JwtUtil;
+import org.example.reservationservice.user.client.UserClient;
 
 import java.util.List;
 
@@ -13,11 +16,21 @@ import java.util.List;
 @RequestMapping("/reservations")
 @RequiredArgsConstructor
 public class ReservationController {
-
     private final ReservationService reservationService;
+    private final JwtUtil jwtUtil;
+    private final UserClient userClient;
 
     @PostMapping
-    public ResponseEntity<Reservation> create(@RequestBody ReservationDTO dto) {
+    public ResponseEntity<Reservation> create(
+            @RequestBody ReservationDTO dto,
+            @RequestHeader("Authorization") String authHeader) {
+
+        String token = authHeader.substring(7);
+        String username = jwtUtil.extractUsername(token);
+
+        Long userId = userClient.getUserIdByUsername(username);
+        dto.setUserId(userId);
+
         return ResponseEntity.ok(reservationService.createReservation(dto));
     }
 
---
+++ reservation-service/src/main/java/org/example/reservationservice/reservation/security/JwtUtil.java
@@ -0,0 +1,70 @@
+package org.example.reservationservice.reservation.security;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private static SecretKey SECRET_KEY;
+    private SecretKey secretKey;
+
+    private static final String SECRET = "your-256-bit-secret-your-256-bit-secret"; // minimum 256 bitów (32 znaki)
+
+    @PostConstruct
+    public void init() {
+        // Inicjalizujemy klucz z zakodowanego Stringa (np. base64 lub UTF-8)
+        SECRET_KEY = Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8));
+    }
+
+
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))  // 1 godzina
+                .signWith(SECRET_KEY)
+                .compact();
+    }
+
+    public Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public String extractUsername(String token) {
+        return extractClaims(token).getSubject();
+    }
+
+    public boolean isTokenExpired(String token) {
+        return extractClaims(token).getExpiration().before(new Date());
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    public String extractUsernameFromRequest(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+            return extractUsername(token);
+        }
+        throw new RuntimeException("Missing token");
+    }
+}
