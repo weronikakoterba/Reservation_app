6d107d0f36af32325419f4250d90e2c257dd5230
julita.zamroczynska <julita.zamroczynska@icloud.com>
2025-06-08 20:54:39 +0200

    notification service done with real email

--- notification-service/src/main/java/org/example/notificationservice/service/NotificationService.java
+++ notification-service/src/main/java/org/example/notificationservice/service/NotificationService.java
@@ -1,13 +1,36 @@
 package org.example.notificationservice.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.example.notificationservice.dto.NotificationDTO;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationService {
 
+    private final JavaMailSender mailSender;
+
+    public NotificationService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
     public void sendNotification(NotificationDTO request) {
-        System.out.println(">>> Powiadomienie do: " + request.getRecipient());
-        System.out.println(">>> Wiadomość: " + request.getMessage());
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setTo(request.getRecipient());
+            helper.setSubject("Nowa wiadomość z aplikacji rezerwacyjnej");
+            helper.setText(request.getMessage(), true); // HTML enabled
+
+            mailSender.send(message);
+            System.out.println("Wysłano e-mail do: " + request.getRecipient());
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            System.err.println("Błąd podczas wysyłania wiadomości: " + e.getMessage());
+        }
     }
 }
